ef937c55e18eb390ac2913afc28eed45d5690a58
patrick85081 <patrick85081@gmail.com>
2018-01-17 21:18:28 +0800

    Add Debug DB.

---
+++ app/src/main/java/com/example/androidroomdemo/App.kt
@@ -0,0 +1,37 @@
+package com.example.androidroomdemo
+
+import android.app.Application
+import android.content.Context
+import android.widget.Toast
+import java.lang.reflect.Method
+
+/**
+ * Created by user on 2018/1/17.
+ */
+class App : Application()
+{
+    override fun onCreate()
+    {
+        super.onCreate()
+
+        showDebugDBAddressLogToast(this);
+    }
+
+    private fun showDebugDBAddressLogToast(context: Context)
+    {
+        if (BuildConfig.DEBUG)
+        {
+            try
+            {
+                val debugDB: Class<*> = Class.forName("com.amitshekhar.DebugDB");
+                val getAddressLog: Method = debugDB.getMethod("getAddressLog");
+                val value: Any = getAddressLog.invoke(null);
+                Toast.makeText(context, value.toString(), Toast.LENGTH_LONG).show();
+            }
+            catch (ignore: Exception)
+            {
+
+            }
+        }
+    }
+}
